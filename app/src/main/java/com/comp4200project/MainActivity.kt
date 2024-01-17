15938891e69e6e214a58a86f9110b84b61c99bcd
Cam Vandy <88672691+camvandy@users.noreply.github.com>
2024-01-16 19:12:46 -0500

    Updated Greeting Line

--- app/src/main/java/com/comp4200project/MainActivity.kt
+++ app/src/main/java/com/comp4200project/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Comp-4200")
                 }
             }
         }
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Comp4200projectTheme {
-        Greeting("Android")
+        Greeting("Comp-4200")
     }
 }
