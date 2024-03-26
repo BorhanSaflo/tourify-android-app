fd222848e4d98e935415958f3b30a25b71e55431
Yousef <karty@uwindsor.ca>
2024-03-26 13:00:31 -0400

    Added Profile Fragment Functionality

--- app/src/main/java/com/tourify/ui/user/profile/ProfileFragment.kt
+++ app/src/main/java/com/tourify/ui/user/profile/ProfileFragment.kt
@@ -1,12 +1,21 @@
 package com.tourify.ui.user.profile
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.tourify.databinding.FragmentProfileBinding
+import com.tourify.api.RetrofitClient
+import com.tourify.models.UserInfoResponse
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileFragment : Fragment() {
 
@@ -15,6 +24,10 @@ class ProfileFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var emailTextView: TextView
+    private lateinit var usernameTextView: TextView
+    private lateinit var passwordTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,7 +40,27 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        emailTextView = binding.userInfoEmail
+        usernameTextView = binding.userInfoUsername
+        passwordTextView = binding.userInfoPassword
+
         return root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            val response = RetrofitClient.apiService.getUserInfo()
+            if(response.isSuccessful && response.body() != null) {
+                val userInfo = response.body()!!
+                emailTextView.text = userInfo.userInfo.email
+                usernameTextView.text = userInfo.userInfo.name
+            } else {
+                Log.d("ProfileFragment", "Failed to get user info")
+            }
+        }
     }
 
     override fun onDestroyView() {
