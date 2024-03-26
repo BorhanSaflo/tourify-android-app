de5c2dcba7aad7663d3ff34c56426c4564a622a8
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2024-03-25 22:29:16 -0400

    feat: Dynamically Display Username on Homepage

--- app/src/main/java/com/tourify/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/tourify/ui/home/HomeViewModel.kt
@@ -1,13 +1,26 @@
 package com.tourify.ui.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.tourify.models.UserInfoResponse
+import com.tourify.repository.MainRepository
+import com.tourify.utils.ApiResponse
+import com.tourify.viewmodels.BaseViewModel
+import com.tourify.viewmodels.CoroutinesErrorHandler
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val mainRepository: MainRepository,
+): BaseViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _userInfoResponse = MutableLiveData<ApiResponse<UserInfoResponse>>()
+    val userInfoResponse = _userInfoResponse
+
+    fun getUserInfo(coroutinesErrorHandler: CoroutinesErrorHandler) = baseRequest(
+        _userInfoResponse,
+        coroutinesErrorHandler,
+    ) {
+        mainRepository.getUserInfo()
     }
-    val text: LiveData<String> = _text
 }
